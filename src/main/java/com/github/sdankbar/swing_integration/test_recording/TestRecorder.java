d57caa2cd0aeb0b1315ae51726ad402b8eb20f62
Stephen Dankbar <43022481+sdankbar@users.noreply.github.com>
2024-03-12 21:07:14 -0500

    Initial commit of event recorder.

---
+++ src/main/java/com/github/sdankbar/swing_integration/test_recording/TestRecorder.java
@@ -0,0 +1,234 @@
+package com.github.sdankbar.swing_integration.test_recording;
+
+import java.awt.AWTEvent;
+import java.awt.Graphics2D;
+import java.awt.Toolkit;
+import java.awt.Window;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.swing.FocusManager;
+
+public class TestRecorder {
+
+	private class RecordedEvent {
+		private final AWTEvent event;
+		private final Instant eventTime;
+		private final File screenshotFile;
+		private final boolean motionEvent;
+
+		public RecordedEvent(final AWTEvent event, final Instant eventTime) {
+			this.event = event;
+			this.eventTime = eventTime;
+			screenshotFile = null;
+			motionEvent = false;
+		}
+
+		public RecordedEvent(final AWTEvent event, final Instant eventTime, final boolean isMotion) {
+			this.event = event;
+			this.eventTime = eventTime;
+			screenshotFile = null;
+			motionEvent = isMotion;
+		}
+
+		public RecordedEvent(final Instant eventTime, final File screenshotFile) {
+			event = null;
+			this.eventTime = eventTime;
+			this.screenshotFile = screenshotFile;
+			motionEvent = false;
+		}
+	}
+
+	private static final int MOVE_SAMPLE_RATE = 250;
+
+	private final List<RecordedEvent> recordedEvents = new ArrayList<>();
+
+	private boolean isRecording = false;
+	private Instant startTime = Instant.EPOCH;
+	private File recordingDir = new File(".");
+	private Instant lastMouseMoveTime = Instant.EPOCH;
+
+	public TestRecorder() {
+		Toolkit.getDefaultToolkit().addAWTEventListener(event -> {
+			handleKeyEvent(event);
+		}, AWTEvent.KEY_EVENT_MASK);
+
+		Toolkit.getDefaultToolkit().addAWTEventListener(event -> {
+			handleMouseMotionEvent(event);
+		}, AWTEvent.MOUSE_MOTION_EVENT_MASK);
+
+		Toolkit.getDefaultToolkit().addAWTEventListener(event -> {
+			handleMouseEvent(event);
+		}, AWTEvent.MOUSE_EVENT_MASK | AWTEvent.MOUSE_WHEEL_EVENT_MASK);
+	}
+
+	private void handleMouseMotionEvent(final AWTEvent event) {
+		final Instant now = Instant.now();
+		final long milli = now.toEpochMilli();
+		if ((milli - lastMouseMoveTime.toEpochMilli()) > MOVE_SAMPLE_RATE && isRecording) {
+			final RecordedEvent rec = new RecordedEvent(event, now, true);
+			lastMouseMoveTime = now;
+			recordedEvents.add(rec);
+		}
+	}
+
+	private void handleMouseEvent(final AWTEvent event) {
+		if (isRecording) {
+			final RecordedEvent rec = new RecordedEvent(event, Instant.now());
+			recordedEvents.add(rec);
+		}
+	}
+
+	private void handleKeyEvent(final AWTEvent event) {
+		final KeyEvent key = (KeyEvent) event;
+
+		if (key.getID() == KeyEvent.KEY_PRESSED) {
+			if (key.getKeyCode() == KeyEvent.VK_F11) {
+				// Ignore
+			} else if (key.getKeyCode() == KeyEvent.VK_F10) {
+				// Ignore
+			} else if (isRecording) {
+				final RecordedEvent rec = new RecordedEvent(event, Instant.now());
+				recordedEvents.add(rec);
+			}
+			lastMouseMoveTime = Instant.EPOCH;
+		} else if (key.getID() == KeyEvent.KEY_RELEASED) {
+			if (key.getKeyCode() == KeyEvent.VK_F11) {
+				if (isRecording) {
+					saveTestSteps();
+				} else {
+					System.out.println("Start recording");
+					startTime = Instant.now();
+					recordedEvents.clear();
+
+					recordingDir = new File("recording_" + startTime.toEpochMilli());
+					recordingDir.mkdir();
+				}
+				isRecording = !isRecording;
+
+				lastMouseMoveTime = Instant.EPOCH;
+			} else if (key.getKeyCode() == KeyEvent.VK_F10 && isRecording) {
+
+				final BufferedImage windowImage = takeScreenshot();
+				if (windowImage != null) {
+					final String now = Long.toString(Instant.now().getEpochSecond());
+					final String fileName = "screenshot_" + now + ".png";
+					final File filePath = new File(recordingDir, fileName);
+					try {
+						ImageIO.write(windowImage, "PNG", filePath);
+						final RecordedEvent rec = new RecordedEvent(Instant.now(), filePath);
+						recordedEvents.add(rec);
+					} catch (final IOException e) {
+						e.printStackTrace();// TODO
+					}
+				}
+				lastMouseMoveTime = Instant.EPOCH;
+			} else if (isRecording) {
+				final RecordedEvent rec = new RecordedEvent(event, Instant.now());
+				recordedEvents.add(rec);
+			}
+			lastMouseMoveTime = Instant.EPOCH;
+		}
+	}
+
+	private BufferedImage takeScreenshot() {
+		final Window w = FocusManager.getCurrentManager().getActiveWindow();
+		final BufferedImage i = new BufferedImage(w.getWidth(), w.getHeight(), BufferedImage.TYPE_INT_ARGB);
+		final Graphics2D g = i.createGraphics();
+		w.paint(g);
+		g.dispose();
+		return i;
+	}
+
+	private void saveTestSteps() {
+		final File output = new File(recordingDir, "TemplateTest.java");
+		try (BufferedWriter w = new BufferedWriter(new FileWriter(output))) {
+
+			w.write("import java.io.File;\n");
+			w.write("import java.time.Duration;\n");
+			w.write("import org.junit.After;\n");
+			w.write("import org.junit.Test;\n");
+			w.write("import java.awt.Robot;\n");
+			w.write("\n");
+			w.write("public class IntegrationTest {\n");
+			w.write("\n");
+			w.write("\tprivate final String screenshotDir = \"TODO\";\n");
+			w.write("\n");
+			w.write("\t@Before\n");
+			w.write("\tpublic void setup() {\n");
+			w.write("\t\t// TODO run test setup\n");
+			w.write("\t}\n");
+			w.write("\n");
+			w.write("\t@After\n");
+			w.write("\tpublic void finish() {\n");
+			w.write("\t\t// TODO test cleanup\n");
+			w.write("\t}\n");
+			w.write("\n");
+			w.write("\t@Test\n");
+			w.write("\tpublic void test_run() {\n");
+			w.write("\t\tRobot r = new Robot();\n");
+
+			Instant workingTime = startTime;
+			for (final RecordedEvent e : recordedEvents) {
+				if (e.event != null) {
+					if (e.event instanceof KeyEvent) {
+						final KeyEvent key = (KeyEvent) e.event;
+						if (key.getID() == KeyEvent.KEY_PRESSED) {
+							w.write("\t\tr.keyPress(" + key.getKeyCode() + ");\n");
+						} else {
+							w.write("\t\tr.keyRelease(" + key.getKeyCode() + ");\n");
+						}
+					} else if (e.event instanceof MouseWheelEvent) {
+						final MouseEvent mouse = (MouseWheelEvent) e.event;
+						w.write("\t\tr.mouseWheel(" + mouse.getClickCount() + ");\n");
+					} else if (e.event instanceof MouseEvent) {
+						final MouseEvent mouse = (MouseEvent) e.event;
+						if (e.motionEvent) {
+							w.write("\t\tr.mouseMove(" + mouse.getX() + ", " + mouse.getY() + ");\n");
+						} else if (mouse.getID() == MouseEvent.MOUSE_PRESSED) {
+							w.write("\t\tr.mouseMove(" + mouse.getX() + ", " + mouse.getY() + ");\n");
+							w.write("\t\tr.mousePress(" + mouse.getButton() + ");\n");
+						} else if (mouse.getID() == MouseEvent.MOUSE_RELEASED) {
+							w.write("\t\tr.mouseMove(" + mouse.getX() + ", " + mouse.getY() + ");\n");
+							w.write("\t\tr.mouseRelease(" + mouse.getButton() + ");\n");
+						} else if (mouse.getID() == MouseEvent.MOUSE_WHEEL) {
+							w.write("\t\tr.mouseWheel(" + mouse.getButton() + ");\n");
+						}
+					}
+
+					final long milli = Duration.between(workingTime, e.eventTime).toMillis();
+					w.write("\t\tr.delay(" + milli + ");\n");
+
+					workingTime = e.eventTime;
+				} else {
+					final long milli = Duration.between(workingTime, e.eventTime).toMillis();
+					// TODO
+					w.write("\t\ttools.compareWindowToImage(new File(screenshotDir, \"" + e.screenshotFile
+							+ "\"), Duration.ofMillis(" + milli + "));\n");
+					workingTime = e.eventTime;
+				}
+			}
+
+			w.write("\t}\n");
+			w.write("\n");
+			w.write("}\n");
+
+			w.close();
+			System.out.println("Done writing test");
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
